b56e383af7779518b2df80a496b9c5f5f1d58923
gs-rbarman <rbarman@gainsight.com>
2015-02-28 22:33:47 +0530

    githubapp: modif files

--- javascriptcodingsessions/02232015jagadeeshGithubBackbone/js/oraclev2.1.js
+++ javascriptcodingsessions/02232015jagadeeshGithubBackbone/js/oraclev2.1.js
@@ -1,7 +1,9 @@
+(function(window, _, $, Backbone){
 
 var UserModel = Backbone.Model.extend({
+
 	urlRoot: "https://api.github.com/",
-	url: "",
+
 	updateUrl:function(x){
 		this.url = this.urlRoot + 'users/' + x;
 		return this.url;
@@ -10,40 +12,120 @@ var UserModel = Backbone.Model.extend({
 
 var UserView = Backbone.View.extend({
 	initialize: function(){
-		$('#derm').css('display', 'none');
-		var self = this;
-		var handle = $('#profSch');
-		handle.on('change', function(){
-            var userId = handle.val();
-        	var str = 'https://api.github.com/' + 'users/' + userId;
-        	self.model.updateUrl(userId);
-        	self.model.fetch();
-        });
-        
-        this.model.on('change', function(){
-	    	self.render();
+        this.listenTo(this.model, 'change', function(){
+	    	this.render();
 	    });
 	},
 	events: {
 	        	
 	},
+    updateUser:function(user){
+    	var userId = user;
+    	var str = 'https://api.github.com/' + 'users/' + userId;
+    	this.model.updateUrl(userId);
+    	this.model.fetch();
+    },
 	render: function(){
-		var domContainer = this.$el.parents('.wrapper').find('.epiDerm .vcard');
 		var data = this.model;
-		domContainer.find('.vcard-fullname').html(data.get('name'));
-        domContainer.find(".userImg").attr('src', data.get('avatar_url'));
-	    domContainer.find(".vcard-username").html(data.get('login'));
-	    // this.$el.find(".userImg").attr('src', data.get("avatar_url"));
-	    domContainer.find(".vcard-details #cmpny").html(data.get('company'));
-	    domContainer.find(".vcard-details #hloc").html(data.get('location'));
-	    domContainer.find(".join-date").attr('datetime', data.get('created_at'));
-	    domContainer.find(".vcard-stats a:nth-child(1) strong").html(data.get('followers'));
-	    domContainer.find(".vcard-stats a:nth-child(3) strong").html(data.get('following'));
-		
+		this.$('.vcard-fullname').html(data.get('name'));
+        this.$(".userImg").attr('src', data.get('avatar_url'));
+	    this.$(".vcard-username").html(data.get('login'));
+	    // this.$(".userImg").attr('src', data.get("avatar_url"));
+	    this.$(".vcard-details #cmpny").html(data.get('company'));
+	    this.$(".vcard-details #hloc").html(data.get('location'));
+	    this.$(".join-date").attr('datetime', data.get('created_at'));
+	    this.$(".vcard-stats a:nth-child(1) strong").html(data.get('followers'));
+	    this.$(".vcard-stats a:nth-child(3) strong").html(data.get('following'));
 	}
 });
 
-var v = new UserView({
-	el: '#profSch',
-	model: new UserModel({})
+
+
+
+var RepoModel = Backbone.Model.extend({
+    urlRoot:"https://api.github.com/",
+    updateUrl:function(x, y){
+		this.url = this.urlRoot + 'repos/' + x + '/' + y;
+		return this.url;
+	}
 });
+
+var ReposCollection = Backbone.Collection.extend({
+    model: RepoModel,
+    urlRoot:"https://api.github.com/",
+	updateUrl:function(x){
+		this.url = this.urlRoot + 'users/' + x + '/repos';
+		return this.url;
+	}
+});
+
+var ReposView = Backbone.View.extend({
+    initialize:function(){
+
+    },
+    updateUser:function(user){
+
+    	var userId = user;
+        	var str = 'https://api.github.com/' + 'users/' + userId;
+        	this.collection.updateUrl(userId);
+        	this.collection.fetch({
+	            success:function(){
+	                this.render();                
+	            }.bind(this),
+	            error: function(data,err){
+	                console.info(err.responseJSON.message);
+	            }
+       		});
+
+    },
+    render:function(){
+    	var self = this.$(".repo-list");
+    	console.log(self);
+		this.collection.each(function(item){
+		    self.append(repoList);
+            self.find(".repo-list-item:last").append(repoHead);
+            self.find(".repo-list-item:last").append(repoDesc);
+            self.find(".repo-list-item:last").append(repoTime);
+
+            self.find(".repo-list-name:last a").html(item.get("name"));
+            // $(".repo-list-name:last a").attr('href', 'repo.html');
+            self.find(".repo-list-description:last").html(item.get('description'));
+            self.find(".repo-list-meta:last time").html(item.get('updated_at'));
+		});
+    },
+    events:{
+            
+    }
+});
+
+var GitHubApp = Backbone.View.extend({
+	initialize:function(){
+
+		this._userView  = new UserView({
+			el: this.$el.find(".vcard"),
+			model: new UserModel()
+		});
+
+		this._reposView = new ReposView({
+		    el: this.$el.find(".repo-tab"),
+		    collection: new ReposCollection()
+		});
+
+	},
+	events:{
+		"change #profSch":function(evt){
+			this._userView.updateUser(evt.currentTarget.value);
+			this._reposView.updateUser(evt.currentTarget.value);
+		}
+	}
+});
+
+var _githubApp = new GitHubApp({
+	el:$(".wrapper")[0]
+});
+
+var _githubApp = new GitHubApp({
+	el:$(".wrapper")[1]
+});
+
+})(window, _, jQuery, Backbone);
